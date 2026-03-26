9b5daacf1962157b463e3e090fe6492079736dbd
al99nc <jaafarhamed25@gmail.com>
2026-03-27 01:59:21 +0300

    fixed cors1

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://84.235.244.210:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
